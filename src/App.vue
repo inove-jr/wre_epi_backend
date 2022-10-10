b01c1b3b6607f8cc9e856fed97fdfcc3e6140ec6
Ramon Barros <cabrobas99@gmail.com>
2022-10-10 19:22:37 -0300

    compenente Carrosel

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main">
+    <CarrouselComponent/>
+
+  </div>
+</template>
+
+<script>
+import CarrouselComponent from "./components/CarrouselComponent.vue";
+
+export default {
+  name: 'App',
+  props:[
+   
+  ],
+  
+  components: {
+    CarrouselComponent,
+    
+},
+}
+
+</script>
+
+<style>
+  body{
+    margin: 0;
+
+  }
+#app {
+  margin: 0px;
+  padding: 0px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  height: 100%;
+  width: 100%;
+  background-color: grey;
+}
+.vueperslides--fixed-height { height: 200px; }
+</style>
