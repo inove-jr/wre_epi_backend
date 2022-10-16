f9e82e116003116dac2bf5d9a7874e2ef232035f
Edon Souza <e.brendon30@outlook.com>
2022-10-16 18:19:32 -0300

    Componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
+  <HeaderComponent/>
   <router-view/>
+  
 </template>
 
+<script>
+import HeaderComponent from './components/header.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+}
+}
+</script>
+
 <style>
+  
 </style>
