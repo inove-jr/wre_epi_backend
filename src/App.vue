4c18dbf3795a7f08dd3a6c6c9ff0cbd906aafe00
julyaW11 <julyacomp@gmail.com>
2022-10-24 18:33:03 -0300

    componente footer finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
+  <FooterComponent/>
   <router-view/>
 </template>
 
+<script>
+  import FooterComponent from './components/footer.vue'
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      FooterComponent,
+  }
+  }
+  </script>
+
 <style>
+
+body{ background-color:  #FFFFFF; 
+
+}
+footer{background-color: #3A9E3E ;
+  color: #FFFFFF; 
+}
 </style>
---
+++ src/components/footer.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="footer">
+
+      <div class="contato"> 
+            <h4>Contato:</h4>
+             <p>E-mail: wreppig@gmail.com <br>
+                Telefone: 87 9604-8688
+             </p>
+        </div>
+
+        <div class="empresa"> 
+            <p>
+                WRE-EPI &reg; - 2022 <br>
+                Vale do São Francisco 
+            </p>
+        </div>
+
+    </div>
+
+
+  </template>
+  
+  <style>
+
+  body{ background-color:  #FFFFFF; 
+  
+  }
+  .footer{background-color: #3A9E3E ;
+    color: #FFFFFF;
+    padding: 12px;
+
+    display:grid; 
+    grid-template-columns: 1fr 1fr 1fr ;
+    grid-gap: 0 0;
+  }
+
+
+  .contato {
+    margin-top: 12px;
+    margin-left: 90px;
+    width: 200px;
+  }
+
+  .empresa{
+    margin-top: 18px;
+    text-align: center;
+
+  }
+
+  @media (max-width: 800px) {
+      .footer-col{
+        width:50%;
+        margin-bottom: 30px;
+      }
+  }
+
+  @media (max-width: 600px){
+  .footer-col{
+    width: 100%;
+  }
+}
+
+  </style>
+
+
+
