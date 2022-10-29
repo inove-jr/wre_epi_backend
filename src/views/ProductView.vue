b2188a0232bb4906153fd10bd5e7ca3a3b675cf7
Ramon Barros <cabrobas99@gmail.com>
2022-10-29 00:01:54 -0300

    Player de Video

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -21,7 +21,9 @@
               "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
           </div>
           <div class="video">
-            
+            <iframe 
+              src="https://www.youtube.com/embed/j5a0jTc9S10">
+            </iframe>
           </div>
         </div>
       </div>
@@ -92,5 +94,13 @@
   .item2 {
     grid-column-start: 2;
     grid-column-end: 2;
+  }
+
+  .video{
+    margin: 3em;
+  }
+  .video iframe{
+    height: 20em;
+    width: 35em;
   }
 </style>
