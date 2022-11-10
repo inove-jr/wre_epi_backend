2a324d5fd78171b32b9ed8969d7e6e30bd17329c
RonildoLB <ronildo_lacerda@hotmail.com>
2022-11-10 19:46:16 -0300

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
 
 const routes = [
   {
@@ -14,11 +15,13 @@ const routes = [
     component: ProductView
   },
   {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
